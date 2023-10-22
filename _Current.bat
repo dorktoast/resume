03f99ed0150789e61ad1c0c9f048a05a51c5cee4
DorkToast <sam@gib.games>
2023-10-22 10:22:39 -0600

    Initial Commit

---
+++ _Current.bat
@@ -0,0 +1,139 @@
+.SS SAM SWICEGOOD(1)               USER COMMANDS                SAM SWICEGOOD(1)
+
+.SH NAME
+Sam Swicegood \- Project Manager, Producer & Problem Solver
+
+.SH SYNOPSIS
+.B samswicegood
+[I contact options]
+
+.SH DESCRIPTION
+Project manager with 15 years of leadership and management experience in software products across multifunctional teams.
+
+.SH CONTACT
+Email: \fI resume@samswicegood.com \fR
+
+.SH PROFESSIONAL SKILLS
+Strengths:
+.RS
+Project management, design, technical writing, debugging, infrastructure, security, VR
+.RE
+
+Software:
+.RS
+Unity3d, C#, AWS, Adobe Suite, Autodesk Maya, MS Office, email, HTML, G-Suite, OpenOffice + more
+.RE
+
+.SH SELECTED EXPERIENCE
+
+.RS
+.B Executive Producer
+\fR
+GIB Studio, June 2021-Present
+
+Prioritized product requirements and steered product development roadmap.
+Managed product vision and strategy with accountability for goals.
+Engaged with industry leaders like Facebook/Meta to explore VR trends.
+Conducted post-release research to inform direction and goals.
+
+.B Associate Producer
+\fR
+Moddey Dhoo Software, Oct 2017-June 2021
+
+Developed project management processes to improve team efficiency.
+Managed task fulfillment for cross-disciplinary teams across projects.
+Assisted in software and hardware installations for new users.
+Maintained communication to support team goals and collaboration.
+
+.B Sr. Brand Marketing Copywriter
+\fR
+Confidential, March 2019-April 2020
+
+Collaborated with designers and leadership on project rollouts.
+Maintained inventory of digital assets and physical marketing materials.
+Crafted original concepts based on established voice/tone.
+Managed database related to user data, ensuring PII data policy compliance.
+
+.B Technical Analyst II
+\fR
+CDK Global, February 2016-March 2019
+
+Responded to inquiries via a ticket-based priority system.
+Managed inventory of software and hardware to meet client needs.
+Engaged with third-party vendors on collaborative projects.
+Assisted in the migration of over 3,000 users from an aging database.
+
+.B IT Manager
+\fR
+Gregory S Young Co. LPA, November 2013-December 2015
+
+Handled routine and urgent IT inquiries using a ticket system.
+Assisted with data migration ensuring ABA standards compliance.
+Managed office moves, coordinating technical and logistical tasks.
+Maintained inventory of hardware, software licenses, and compliance data.
+
+.RE
+
+.SH EDUCATION
+
+.RS
+.B Associate of Arts – Communications and PR
+\fR
+Anne Arundel Community College
+\ 
+Gained foundational knowledge in communication theories and practices. Participated in several group projects emphasizing PR campaign strategies.
+
+.B Bachelor of Arts – Mass Media & Communications
+\fR
+University of Cincinnati
+\
+Explored various mass media platforms and communication techniques. Completed a thesis on the impact of video games on narrative storytelling.
+
+.RE
+
+.SH SELECTED CERTIFICATIONS
+
+.RS
+.B Data Scientist Certification
+\fR
+Harvard University (Edx)
+\
+Acquired skills in data analysis, machine learning, and statistical inference. Engaged in practical exercises using real-world data sets.
+
+.B Six Sigma - Yellow Belt
+\fR
+CSSC
+\
+Learned the basics of Six Sigma principles. Focused on improving quality and efficiency in production processes.
+
+.B MS Office Specialist
+\fR
+Microsoft
+\
+Demonstrated advanced skills in Microsoft Office Suite. Became proficient in creating intricate documents, spreadsheets, and presentations.
+
+.B Blueprint
+\fR
+Facebook, Inc.
+\
+Trained in utilizing Facebook for business marketing and advertising strategies. Developed skills in audience targeting and ad optimization.
+
+.B Digital Marketing Garage
+\fR
+Google
+\
+Received comprehensive training in digital marketing strategies. Gained insights into SEO, SEM, and content marketing.
+
+.B Oculus Growth - Member
+\fR
+Oculus
+\
+Engaged with the Oculus community to understand VR trends and development. Participated in forums and workshops to enhance VR content creation skills.
+
+.RE
+
+.SH AUTHOR
+Written by Sam Swicegood.
+
+.SH NOTES
+Resume version 1.0. Last updated October 2023.
